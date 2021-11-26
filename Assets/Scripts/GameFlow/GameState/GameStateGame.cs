730da397efeee0116940b6358f2ab7012f12d9d1
Santiago Sala <aa2793@student.jamk.fi>
2021-11-27 00:11:49 +0200

    Added ui for coin count and score.

--- Assets/Scripts/GameFlow/GameState/GameStateGame.cs
+++ Assets/Scripts/GameFlow/GameState/GameStateGame.cs
@@ -1,9 +1,29 @@
+using TMPro;
+using UnityEngine;
+
 public class GameStateGame : GameState
 {
+    public GameObject gameUI;
+    [SerializeField] private TextMeshProUGUI coinCount;
+    [SerializeField] private TextMeshProUGUI scoreCount;
+
     public override void Construct()
     {
+        base.Construct();
         GameManager._Instance.gameMotor.ResumePlayer();
         GameManager._Instance.ChangeCamera(VirtualCameras.Game);
+
+        coinCount.text = "XTBD";
+        scoreCount.text = "XTBD";
+
+
+        gameUI.SetActive(true);
+
+    }
+
+    public override void Destruct()
+    {
+        gameUI.SetActive(false);
     }
 
     public override void UpdateState()
